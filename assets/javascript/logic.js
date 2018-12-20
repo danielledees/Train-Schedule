91617e696b827794929426d9b408ac8b01371602
Danielle Dees <d.danielle83@gmail.com>
2018-12-19 20:12:15 -0500

    homework

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -22,16 +22,19 @@ var config = {
     //user input
 
     var trainName = $("#train-name-input").val().trim();
-    var destination = $("#destination-input").val().trim();
+    var newDest = $("#destination-input").val().trim();
     var firstTrain = $("#first-train-input").val().trim();
-    var freq = $("#freq-input").val().trim();
+    var newFreq = $("#freq-input").val().trim();
+
+    //object to hold new train input
 
     var newTrain = {
       train: trainName,
-      destination: destination,
+      destination: newDest,
       first: firstTrain,
-      freq: freq,
+      freq: newFreq,
     };
+
     //pushes user input to database
     database.ref().push(newTrain);
 
@@ -40,7 +43,7 @@ var config = {
     console.log(newTrain.first);
     console.log(newTrain.freq);
 
-    alert("new train added nicely");
+    alert("new train added");
 
     //clear all text boxes
     $("#train-name-input").val("");
@@ -48,4 +51,62 @@ var config = {
     $("#first-train-input").val("");
     $("#freq-input").val("");
 
-  })
+  });
+
+
+ 
+
+  database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+
+    //store in variable
+    var trainName =  snapshot.val().train;
+    var newDest =  snapshot.val().destination;
+    var firstTrain  = snapshot.val().first;
+    var newFreq = snapshot.val().freq;
+    //14:13
+    // [14,13]
+    var timeSplit = firstTrain.split(':');
+    var formattedTrainTime = moment().hours(timeSplit[0]).minutes(timeSplit[1]);
+    var maxMoment = moment.max(moment(), formattedTrainTime);
+    var timeMinutes;
+    var timeArrival;
+    if(maxMoment === formattedTrainTime) {
+      timeArrival = formattedTrainTime.format("hh:mm A");
+      timeMinutes = formattedTrainTime(moment(), "minutes");
+    } else {
+      var differenceTimes = moment().diff(formattedTrainTime, "minutes");
+      var timeRemainder = differenceTimes % newFreq
+      timeMinutes = newFreq - timeRemainder
+      timeArrival = moment().add(timeMinutes, "m").format("hh:mm A");
+    }
+    //print new train info
+    console.log(trainName);
+    console.log(newDest);
+    console.log(firstTrain);
+    console.log(newFreq);
+    console.log(timeArrival, "This time should be form")
+
+
+  //create new row
+  var newRow = $("<tr>").append(
+    $("<th>").text(trainName),
+    $("<td>").text(newDest),
+    $("<td>").text(newFreq),
+    $("<td>").text(timeArrival),
+    $('<td>').text(timeMinutes),
+   
+    
+  );
+
+  //update html with new row
+  $("#train-table > tbody").append(newRow);
+
+
+  //need to use moment.js to format time in military time and calculate how many minutes until the next train arrives
+
+
+
+
+});
+  
